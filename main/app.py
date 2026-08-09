12415e48b4aaef31f4157bc3f5aa4fe682503b57
Aust Kaetzchen <austkaetzchen@confoederatio.org>
2026-08-09 10:50:39 +0000

    refix layout

--- main/app.py
+++ main/app.py
@@ -69,8 +69,8 @@ def draw ():
   if uploaded_file is not None:
     reader_obj = loadReader()
     
-    col1, col2, col3 = st.columns(2)
-    col4, col5, col6 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
+    col4, col5, col6 = st.columns(3)
     
     with col1:
       st.subheader("1. Original Map")
